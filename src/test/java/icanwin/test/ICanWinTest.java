8ae36776a3c9f072dff625b94979080d16321c51
Dzmitry Dobysh <Dzmitry_Dobysh@epam.com>
2022-05-23 19:04:35 +0300

    correcting mistakes

--- src/test/java/icanwin/test/ICanWinTest.java
+++ src/test/java/icanwin/test/ICanWinTest.java
@@ -1,23 +1,51 @@
 package icanwin.test;
 
+import icanwin.page.PasteBinMainPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class ICanWinTest extends EnterAllInfoTest {
-    @Test(description = "Check if submitted paste title matches initial paste title")
-    public void checkSubmittedPastePageTitle() {
-        WebElement titleSubmittedPaste = waitForElement(By.xpath(SUBMITTED_PASTE_TITLE_LOCATOR));
-        String actualPageTitle = titleSubmittedPaste.getText();
+import java.time.Duration;
 
-        Assert.assertEquals(actualPageTitle, NEW_PASTE_TITLE, "Submitted paste title doesn't match initial paste title");
+public class ICanWinTest {
+
+    protected WebDriver driver;
+
+    protected static final String NEW_PASTE_TEXT = "Hello from WebDriver";
+    protected static final String NEW_PASTE_TITLE = "helloweb";
+    protected static final String SUBMITTED_PASTE_TITLE_LOCATOR = "//div[@class='info-top']/h1";
+    PasteBinMainPage pasteBinMainPage;
+
+    @BeforeTest(alwaysRun = true)
+    public void browserSetup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
-    @Test(description = "Check if submitted paste text matches initial text")
-    public void checkSubmittedPasteText() {
-        String actualText = pastebinSubmittedPage.getSubmittedPasteText();
+    @Test(description = "Enter all infor for ICanWinTest")
+    public void enterAllNeededInfo(){
+        pasteBinMainPage = new PasteBinMainPage(driver)
+                .openPage()
+                .typeInNewPasteText(NEW_PASTE_TEXT)
+                .openExpirationDropdown()
+                .selectExpiration10M()
+                .typeInNewPasteTitle(NEW_PASTE_TITLE)
+                .submitNewPaste();
+    }
 
-        Assert.assertEquals(actualText, NEW_PASTE_TEXT, "Submitted paste text doesn't match initial paste text");
+    public WebElement waitForElement(By by) {
+        return new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void browserExit() {
+        driver.quit();
+        driver = null;
     }
 }
